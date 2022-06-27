53ed6f642306d62871283db46efb1d13ce264c8c
JeongSeonggil <dataofsg02@gmail.com>
2022-06-27 15:50:40 +0900

    [#5] feature : Mapper

---
+++ UserService/src/main/java/com/cosmos/userservice/mapper/UserLinkMapper.java
@@ -0,0 +1,18 @@
+package com.cosmos.userservice.mapper;
+
+import com.cosmos.userservice.dto.UserLinkDto;
+import com.cosmos.userservice.jpa.entity.UserLinkEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserLinkMapper {
+    UserLinkMapper INSTANCE = Mappers.getMapper(UserLinkMapper.class);
+
+    UserLinkEntity userDtoToUserLinkEntity(UserLinkDto userLinkDto);
+
+
+    @Mapping(target = "user", ignore = true)
+    UserLinkDto userLinkEntityToUserLinkDto(UserLinkEntity userLinkEntity);
+}
