c4a45970cf74d7d32d2bf1af69d301e702515e8c
JeongSeonggil <dataofsg02@gmail.com>
2022-06-27 14:11:30 +0900

    [#2] feature : ExceptionHandler

--- UserService/src/main/java/com/cosmos/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/cosmos/userservice/UserServiceApplication.java
@@ -1,11 +1,14 @@
 package com.cosmos.userservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient // Eureka Setting
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // DB 연결 없이 Spring 실행 (Test)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
