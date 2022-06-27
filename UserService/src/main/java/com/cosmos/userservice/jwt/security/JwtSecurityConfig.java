496ccf634c14345ecf9666059e0d819d8ebba088
doram9 <dodo0207k@naver.com>
2022-06-27 17:29:50 +0900

    [#9] feature : create Jwt

---
+++ UserService/src/main/java/com/cosmos/userservice/jwt/security/JwtSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cosmos.userservice.jwt.security;
+
+import com.cosmos.userservice.jwt.filter.JwtFilter;
+import com.cosmos.userservice.jwt.provider.TokenProvider;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+//TokenProvider, JwtFilter 를 SecurityConfig에 적용할때 사용
+
+
+    private TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    //JwtFilter를 통해 Security 로직에 필터를 등록
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
